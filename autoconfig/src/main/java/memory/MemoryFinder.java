a4045012524fac1b8c5d094a67ce5d47fd639cf6
Lee Myeongsang <mslee0052@gmail.com>
2024-04-30 09:36:33 +0900

    commit

---
+++ autoconfig/src/main/java/memory/MemoryFinder.java
@@ -0,0 +1,15 @@
+package memory;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MemoryFinder {
+
+    public Memory get() {
+        long max = Runtime.getRuntime().maxMemory();
+        long total = Runtime.getRuntime().totalMemory();
+        long free = Runtime.getRuntime().freeMemory();
+        long used = total - free;
+        return new Memory(used, max);
+    }
+}
