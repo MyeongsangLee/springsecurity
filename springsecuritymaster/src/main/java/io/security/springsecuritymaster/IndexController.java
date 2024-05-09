26a42606950552abf492d4238916e4c091874f23
Lee Myeongsang <mslee0052@gmail.com>
2024-05-09 23:22:01 +0900

    commit

--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/IndexController.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -10,12 +10,14 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
     @GetMapping("/home")
     public String home() {
         return "home";
     }
+
     @GetMapping("/loginPage")
-    public String login() {
+    public String loginPage() {
         return "loginPage";
     }
 }
--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,16 +1,22 @@
 package io.security.springsecuritymaster;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -20,23 +26,29 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(form -> form
-                        .loginPage("/loginPage")
-                        .loginProcessingUrl("/loginProc")
-                        .defaultSuccessUrl("/", true)
-                        .failureUrl("/login")
-                        .usernameParameter("userId")
-                        .passwordParameter("passwd")
-//                        .successHandler((request, response, authentication) -> {
-//                            System.out.println("authentication: " + authentication.getName());
-//                            response.sendRedirect("/home");
-//                        })
-//                        .failureHandler((request, response, exception) -> {
-//                            System.out.println("exception: " + exception.getMessage());
-//                            response.sendRedirect("/login");
-//                        })
-                        .permitAll()
-                );
+            .formLogin(form -> form
+                .loginPage("/loginPage")
+                .loginProcessingUrl("/loginProc") //action 이름
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/failed")
+                .usernameParameter("userId")  //name으로 매칭
+                .passwordParameter("passwd")
+//                .successHandler(new AuthenticationSuccessHandler() {
+//                    @Override
+//                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//                        System.out.println("authentication: " + authentication.getName());
+//                    }
+//                })
+                        .successHandler((request, response, authentication) -> {
+                            System.out.println("authentication: " + authentication.getName());
+                            response.sendRedirect("/home");
+                        })
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("exception: " + exception.getMessage());
+                            response.sendRedirect("/login");
+                        })
+                .permitAll()
+        );
 
         return http.build();
 
